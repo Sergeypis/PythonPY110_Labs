9e92a89c4d17fbc5777f26059e3f5507ab701744
Serega <ser.pisarenko@gmail.com>
2024-05-17 11:46:30 +0300

    ЛАБ 3 выполнено задание 2

--- store/views.py
+++ store/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 
 from django.http import JsonResponse
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from .models import DATABASE
 
 
 def products_view(request):
     if request.method == "GET":
-        return JsonResponse(DATABASE, json_dumps_params={'ensure_ascii': False, 'indent': 4})
+        data = DATABASE.copy()
+        product_id = request.GET.get('id')
+        if product_id is not None:
+            data = [product for product in DATABASE.values() if product.get('id') == int(product_id)]
+            if not data:
+                return HttpResponseNotFound("Данного продукта нет в базе данных")
+        return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 4})
 
 
 def shop_view(request):
